354a8c7aa0650fb1123a63a927613324acdad6b4
Ricky Rhodes <ricky@rickyrhodes.com>
2020-08-05 01:45:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,7 +71,10 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   };
 
-
-  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  var cartTotal = total();
+  
+  cart = [];
+  
+  return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`;
 
 }
